da10f8b0c7457c84c81a3f1e16c2125863cbeda7
Christian <christian.cuneo@libero.it>
2026-05-27 09:22:20 +0200

    ready for depolyment

---
+++ backend/api.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+app.use(express.json())
+const allowed_origin = process.env.FRONTEND_URL || 'http://localhost:5173'
+app.use(cors({origin : allowed_origin}))
+
+
+let idc = 1
+const acts = [{name:'puffing', id:1, done:true}]
+
+const getActs = (req, res) => {
+    return res.status(201).json({acts})
+}
+const addAct = (req, res) => {
+    const name  = req.body.name
+    const newAct = { name, id: idc + 1, done: false }
+    idc ++
+    acts.push(newAct)
+    return res.status(200).json(newAct)
+}
+const deleteAct = (req, res) => {
+    const id = Number(req.params.id)
+    const idx = acts.findIndex(act => act.id === id)
+    acts.splice(idx, 1)
+    return res.status(200).json({message : 'Deleted'})
+}
+const toggleAct = (req, res) => {
+    const id = Number(req.params.id)
+    const act = acts.find(act => act.id === id)
+    act.done = !act.done
+    return res.status(200).json(id)
+}
+
+app.get('/acts', getActs)
+app.post('/acts', addAct)
+app.delete('/acts/:id', deleteAct)
+app.patch('/acts/:id', toggleAct)
+
+app.listen(5173, '0.0.0.0', () => console.log('acts listening on port 5173'))
